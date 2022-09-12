198efaf52d189fe781f5048ab1b2b744a2173617
vivek <krvivi28@gmail.com>
2022-09-12 09:54:12 +0530

    1.0

---
+++ js/client.js
@@ -0,0 +1,47 @@
+const socket = io('http://localhost:5500');
+const form = document.getElementById('send-container');
+const messageInput = document.getElementById('messageInp');
+const messageContainer = document.querySelector(".container");
+var audio = new Audio('noti.mpeg');
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.innerText = message;
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageContainer.append(messageElement);
+    if (position == 'left')
+
+    {
+        audio.play();
+    }
+
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const message = messageInput.value;
+    append(`you:${message}`, 'right');
+    socket.emit('send', message);
+    messageInput.value = ''
+
+});
+
+const name = prompt("enter your name");
+socket.emit('new-user-joined', name);
+
+
+
+
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'left');
+})
+
+
+
+
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+});
+socket.on('left', name => {
+    append(`${data.name} left the chat`, 'left')
+});
